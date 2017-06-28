1b7347b21e942ef2bd8dc526a1826a2c7f7850da
Kamal Mohanan <kamalmohanan2.71@gmail.com>
2017-06-28 14:01:18 +0530

    Gameloop added.

--- client/app.js
+++ client/app.js
@@ -2,14 +2,33 @@
 
 /*jslint browser: true */
 
+let player = {};
+let bullet = {};
+
+function recv(event) {
+	const data = JSON.parse(event.data);
+	Object.assign(bullet, data.bullet);
+	Object.assign(player, data.player);
+	console.log(bullet);
+}
+
 window.onload = function() {
 	const socket = new WebSocket('ws://localhost:8080');
+	const canvas = document.getElementById('canvas');
+	const context = canvas.getContext('2d');
 
-	socket.onopen = function (event) {
-		console.log('event: ', event);
+
+	socket.onopen = function () {
 		socket.send('Hello from the other side.');
 		socket.onmessage = function(event){
-			console.log('event: ', event, 'data: ', event.data);
+			const data = JSON.parse(event.data);
+			const width = data.dim[0], height = data.dim[1];
+			Object.assign(bullet, data.bullet);
+			Object.assign(player, data.player);
+			context.canvas.width = width;
+			context.canvas.height = height;
+
+			socket.onmessage = recv;
 		};
 	};
 };
--- index.js
+++ index.js
@@ -5,26 +5,54 @@ const app = express();
 
 app.use(express.static('client'));
 
-app.use(function (req, res) {
-	res.send('hello');
-});
-
 const http = require('http');
 const server = http.createServer(app);
 
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', function connection(ws, req) {
-	console.log('ws: ', ws);
-	console.log('req: ', req);
 
-	ws.on('message', function incoming(data) {
-		console.log('data: ', data);
-	});
+// Globals
+const canvasDimensions = [ 640, 480 ];
+let bullet = { x: 100, y : 100, dx : 1, dy : 0 }; // dx = 1 pixel per sec
+let player = { x : 0, y : 0 };
+let clients = [];
 
-	ws.send('Hello World');
+wss.on('connection', function connection(ws) {
+	// Send initial game parameters
+	clients.push(ws);
+	ws.send(JSON.stringify({
+		dim : canvasDimensions,
+		bullet : bullet,
+		player : player
+	}));
+
+	ws.on('message', incoming);
 });
+
+function incoming(data) {
+	console.log(data);
+}
+
+
+
+const gameloop = require('node-gameloop');
+
+// Gamestate updation
+gameloop.setGameLoop(function(delta) {
+	bullet.x += bullet.dx * delta;
+}, 1000 / 30);
+
+// Periodically send updates to clients.
+gameloop.setGameLoop(function() {
+	for (let client of clients) {
+		client.send(JSON.stringify({
+			bullet : bullet,
+			player : player
+		}));
+	}
+}, 50);
+
 
 server.listen(8080, function listening() {
 	console.log('listening on 8080...');
