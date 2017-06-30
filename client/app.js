4f58c384f386654cc65c025ebb4b91e1b849b82d
Kamal Mohanan <kamalmohanan2.71@gmail.com>
2017-06-30 13:30:59 +0530

    basic drawing

--- client/app.js
+++ client/app.js
@@ -9,13 +9,26 @@ function recv(event) {
 	const data = JSON.parse(event.data);
 	Object.assign(bullet, data.bullet);
 	Object.assign(player, data.player);
-	console.log(bullet);
 }
+
 
 window.onload = function() {
 	const socket = new WebSocket('ws://localhost:8080');
 	const canvas = document.getElementById('canvas');
 	const context = canvas.getContext('2d');
+
+	const redraw = function() {
+
+		// Hint: Doesnot work with transformations
+		context.clearRect(0, 0, canvas.width, canvas.height);
+
+		// Draw bullet
+		context.fillRect(bullet.x, bullet.y, 10, 10);
+		// Draw player
+		context.fillRect(player.x, player.y, 20, 20);
+
+		requestAnimationFrame(redraw);
+	};
 
 
 	socket.onopen = function () {
@@ -28,7 +41,9 @@ window.onload = function() {
 			context.canvas.width = width;
 			context.canvas.height = height;
 
+			requestAnimationFrame(redraw);
 			socket.onmessage = recv;
+
 		};
 	};
 };
--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const wss = new WebSocket.Server({ server });
 
 // Globals
 const canvasDimensions = [ 640, 480 ];
-let bullet = { x: 100, y : 100, dx : 1, dy : 0 }; // dx = 1 pixel per sec
+let bullet = { x: 100, y : 100, dx : 1000, dy : 0 }; // dx = 1 pixel per sec
 let player = { x : 0, y : 0 };
 let clients = [];
 
@@ -41,6 +41,7 @@ const gameloop = require('node-gameloop');
 // Gamestate updation
 gameloop.setGameLoop(function(delta) {
 	bullet.x += bullet.dx * delta;
+	bullet.x %= canvasDimensions[0];
 }, 1000 / 30);
 
 // Periodically send updates to clients.
